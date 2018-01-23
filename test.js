1567043c1c7d389ad37380766992434a12ba4e4f
Ben Sorohan <ben@backbase.com>
2018-01-23 11:25:57 +0100

    Tested with json-schema-ref-parser (and it works)

--- test.js
+++ test.js
@@ -1,11 +1,31 @@
 const ramlParser = require('raml-1-parser');
 
+let err = false;
+
 ramlParser.loadApi('./api.raml', [], { rejectOnErrors: true })
   .then(() => {
-    console.log('success');
+    console.log('success parsing raml');
   })
   .catch((err) => {
     console.log(err);
-    process.exit(1);
+    err = true;
   });
+
+/*
+const $RefParser = require('json-schema-ref-parser');
+$RefParser.dereference('./schemas/one.json', (err, schema) => {
+  if (err) {
+    console.error(err);
+    err = true;
+  }
+  else {
+    console.log('success dereferencing json');
+    console.log(JSON.stringify(schema, null, 2));
+  }
+});
+*/
+
+if (err) {
+  process.exit(1);
+}
 
